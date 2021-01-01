a42bb503b2814e307d70aabb17c8548abe7abff4
miyozaki <76810815+miyozaki@users.noreply.github.com>
2021-01-01 10:57:05 +0530

    Added ShinobiInputDto.cs

--- Naruto/Models/ShinobiInputDto.cs
+++ Naruto/Models/ShinobiInputDto.cs
@@ -2,6 +2,11 @@ namespace Naruto.Models
 {
     public class ShinobiInputDto
     {
-        
+        public string Slug { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ProfileImage { get; set; }
+        public string CoverImage { get; set; }
     }
 }
